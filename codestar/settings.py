c0dd386d30d242c77beb6c1baa8520eb55265389
Sam Green <sam.green7777@yahoo.com>
2024-09-20 17:53:24 +0000

    add post view

--- codestar/settings.py
+++ codestar/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'blog',
 ]
 
@@ -92,7 +93,8 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.8000-samgree-ithinktherefore-uddw61o5ues.ws-eu116.gitpod.io/",
+    "https://*.codeinstitute-ide.net/",
+    "https://8000-samgree-ithinktherefore-uddw61o5ues.ws-eu116.gitpod.io",
     "https://*.herokuapp.com"
 ]
 
@@ -130,7 +132,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
